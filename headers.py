a52ede02d467ade639679d88d4050efa24fcaefa
lismusa-pp <lis.musa13@gmail.com>
2025-07-02 02:05:33 +0200

    Create headers.py
    For user agents

---
+++ headers.py
@@ -0,0 +1,10 @@
+USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/124.0.0.0 Safari/537.36"
+)
+
+HEADERS = {
+    "User-Agent": USER_AGENT,
+    "Accept-Language": "en-US,en;q=0.9"
+}
